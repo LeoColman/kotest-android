171331074666a1d52b9befb89550a5d631b33afa
SeungHun Choe <13363349+uOOOO@users.noreply.github.com>
2024-06-08 23:19:15 +0900

    Add extention to get value by reflection

--- kotest-extensions-android/src/main/kotlin/br/com/colman/kotest/android/extensions/robolectric/Extensions.kt
+++ kotest-extensions-android/src/main/kotlin/br/com/colman/kotest/android/extensions/robolectric/Extensions.kt
@@ -7,3 +7,9 @@ internal fun Class<*>.getField(fieldClass: Type): Field {
   return declaredFields.firstOrNull { it.type == fieldClass }
     ?: throw IllegalStateException("Not found $fieldClass field.")
 }
+
+internal inline fun <reified T> Class<*>.getValue(obj: Any): T {
+  val field = getField(T::class.java)
+  field.isAccessible = true
+  return field.get(obj) as T
+}
