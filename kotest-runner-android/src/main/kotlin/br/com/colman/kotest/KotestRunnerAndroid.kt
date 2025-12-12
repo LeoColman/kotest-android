a1f390e5b5b89b8ca7f0f43b898703359e0f4f7a
syarihu <info@syarihu.net>
2025-12-12 20:13:58 +0900

    Update kotest-runner-android for Kotest 6 API changes

--- kotest-runner-android/src/main/kotlin/br/com/colman/kotest/KotestRunnerAndroid.kt
+++ kotest-runner-android/src/main/kotlin/br/com/colman/kotest/KotestRunnerAndroid.kt
@@ -1,11 +1,11 @@
 package br.com.colman.kotest
 
-import io.kotest.core.config.AbstractProjectConfig
+import io.kotest.common.KotestInternal
 import io.kotest.core.names.DuplicateTestNameMode
-import io.kotest.engine.extensions.EmptyExtensionRegistry
 import io.kotest.core.spec.Spec
 import io.kotest.engine.TestEngineLauncher
 import io.kotest.engine.config.ProjectConfigResolver
+import io.kotest.engine.extensions.DefaultExtensionRegistry
 import io.kotest.engine.spec.Materializer
 import io.kotest.engine.spec.SpecInstantiator
 import io.kotest.engine.test.names.DefaultDisplayNameFormatter
@@ -14,6 +14,7 @@ import org.junit.runner.Description
 import org.junit.runner.Runner
 import org.junit.runner.notification.RunNotifier
 
+@OptIn(KotestInternal::class)
 class KotestRunnerAndroid(
   private val kClass: Class<out Spec>
 ) : Runner() {
@@ -22,12 +23,15 @@ class KotestRunnerAndroid(
   override fun run(notifier: RunNotifier) {
     runBlocking {
       val listener = JUnitTestEngineListener(notifier)
-      TestEngineLauncher(listener).withClasses(kClass.kotlin).launch()
+      TestEngineLauncher()
+        .withListener(listener)
+        .withClasses(kClass.kotlin)
+        .launch()
     }
   }
 
   override fun getDescription(): Description {
-    val spec = runBlocking { SpecInstantiator(EmptyExtensionRegistry, ProjectConfigResolver()).createAndInitializeSpec( kClass.kotlin).getOrThrow() }
+    val spec = runBlocking { SpecInstantiator(DefaultExtensionRegistry(), ProjectConfigResolver()).createAndInitializeSpec(kClass.kotlin).getOrThrow() }
     spec.duplicateTestNameMode = DuplicateTestNameMode.Warn
     val desc = Description.createSuiteDescription(spec::class.java)
     Materializer().materialize(spec).forEach { rootTest ->
